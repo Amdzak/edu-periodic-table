883aab71f5a2c9cb1fa7112ff96f08ce6c25501c
Amdzak <ahmadzulfikar133@gmail.com>
2026-01-31 13:02:11 +0700

    fix style table

--- app/ElementCard.tsx
+++ app/ElementCard.tsx
@@ -1,20 +1,23 @@
 import { PHASE_COLOR_MAP } from "./colorMap"
 
-function ElementCard({
-  atomicNumber,
-  symbol,
-  name,
-  atomicMass,
-  indName,
-  xpos,
-  ypos,
-  phase,
-  category,
-}) {
-  // fallback logic
+interface ElementCardProps {
+  atomicNumber: number
+  symbol: string
+  name: string
+  indName: string
+  atomicMass: number
+  xpos: number
+  ypos: number
+  phase: string
+  category: string
+}
+
+function ElementCard({atomicNumber, symbol, name, atomicMass, indName, xpos, ypos, phase, category,}: ElementCardProps) {
   const colorClass =
-    PHASE_COLOR_MAP[phase] ||
-    (category === "synthetic" ? PHASE_COLOR_MAP.Synthetic : "bg-gray-300 border-gray-300")
+    PHASE_COLOR_MAP[phase as keyof typeof PHASE_COLOR_MAP] ||
+    (category === "synthetic"
+      ? PHASE_COLOR_MAP.Synthetic
+      : "bg-gray-300 border-gray-300")
 
   const [bgColor, borderColor] = colorClass.split(" ")
 
@@ -24,31 +27,35 @@ function ElementCard({
         gridColumnStart: xpos,
         gridRowStart: ypos,
       }}
-      className={`rounded-xl p-1 ${bgColor} w-28`}
+      className={` ${bgColor} rounded-xl p-1 aspect-3/4 min-w-16 md:min-w-22 max-w-24`}
     >
-      <div className="bg-white rounded-lg h-40 flex flex-col items-center relative">
+      <div className="bg-white rounded-lg h-full flex flex-col items-center relative">
 
         {/* Atomic number */}
-        <span className="absolute top-6 left-6 text-xs font-semibold">
+        <span className="absolute top-1 left-1 text-xs md:font-semibold">
           {atomicNumber}
         </span>
 
         {/* Symbol */}
         <div className="flex-1 flex items-center justify-center">
-          <span className="text-4xl font-extrabold">
+          <span className="md:text-4xl sm:text-xl font-extrabold">
             {symbol}
           </span>
         </div>
 
         {/* Atomic mass */}
-        <span className="absolute bottom-10 left-6 text-xs">
+        <span className="absolute bottom-6 left-1 text-xs md:font-semibold">
           {atomicMass}
         </span>
 
         {/* Footer */}
-        <div className={`w-full text-center border-t-2 ${borderColor} py-1`}>
-          <div className="text-[10px] capitalize">{indName}</div>
-          <div className="text-xs font-semibold uppercase">{name}</div>
+        <div className={`w-full text-center border-t ${borderColor} py-1`}>
+          <div className="text-[9px] capitalize truncate">
+            {indName}
+          </div>
+          <div className="text-[8px] md:text-[9px] font-semibold uppercase truncate">
+            {name}
+          </div>
         </div>
 
       </div>
--- app/PeriodicTable.jsx
+++ app/PeriodicTable.jsx
@@ -3,21 +3,23 @@ import { elements } from "./elements.json"
 
 function PeriodicTable() {
   return (
-    <div className="grid grid-cols-18 gap-2 p-4">
-      {elements.map((el) => (
-        <ElementCard
-          key={el.number}
-          atomicNumber={el.number}
-          symbol={el.symbol}
-          name={el.name}
-          indName={el.IndName}
-          atomicMass={Math.round(el.atomic_mass)}
-          xpos={el.xpos}
-          ypos={el.ypos}
-          phase={el.phase}
-          category={el.category}
-        />
-      ))}
+    <div className="w-full overflow-x-auto">
+      <div className="grid grid-cols-18 gap-3 p-4 min-w-300">
+        {elements.map((el) => (
+          <ElementCard
+            key={el.number}
+            atomicNumber={el.number}
+            symbol={el.symbol}
+            name={el.name}
+            indName={el.IndName}
+            atomicMass={Math.round(el.atomic_mass)}
+            xpos={el.xpos}
+            ypos={el.ypos}
+            phase={el.phase}
+            category={el.category}
+          />
+        ))}
+      </div>
     </div>
   )
 }
